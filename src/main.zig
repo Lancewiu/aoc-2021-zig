64ad5704037b5deed8d124ffd2805a04427548ab
Lawrence Niu <ranchwiu@gmail.com>
2024-06-25 23:25:22 -0400

    complete day 16 part 2
    Learned some interesting things regarding type conversions:
    
    arrays of sub-byte primitives (u4, etc.) are always aligned to the
    nearest byte. Conversion from [_]u4 to [_]u8 required manual
    intervention.

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const assert = std.debug.assert;
 const data = @import("packet.zig");
 
-const IS_TESTING = true;
+const IS_TESTING = false;
 
 fn decode(c: u8) ![4]u1 {
     return switch (c) {
@@ -64,6 +64,7 @@ const Bits = struct {
         for (self.buf[self.i..][0..count], 0..) |bit, i| {
             if (1 == bit) val |= @as(u16, 1) << (count - 1 - std.math.lossyCast(u4, i));
         }
+        self.i +|= count;
         return val;
     }
 
@@ -88,63 +89,80 @@ pub fn main() !void {
     var contexts = std.ArrayList(Context).init(alloc);
     var bits = Bits.init(bitbuf[0..]);
 
+    var version_sum: u64 = 0;
     while (bits.i < bits.buf.len) {
         const istart = bits.i;
-        bits.skip(3);
+        version_sum += try bits.readInt(3);
         const packet_type = try bits.readInt(3);
 
-        if (4 != packet_type) {
-            // op
-            const opid = try std.meta.intToEnum(data.OpId, packet_type);
-            const packmode = try std.meta.intToEnum(
-                data.PackMode,
-                try bits.readInt(1),
-            );
-            const limit: u16 = switch (packmode) {
-                .npackets => try bits.readInt(11),
-                .nbits => try bits.readInt(15),
-            };
-            const bit_count: u16 = @truncate(bits.i - istart);
-            for (contexts.items) |*ctx| {
-                ctx.addBits(bit_count);
-                if (ctx.isComplete()) return error.PacketTruncated;
+        if (4 == packet_type) {
+            // literal
+            var nibble_buf: [16]u4 = undefined;
+            var nibble_count: usize = 0;
+            while (true) {
+                const is_last = 0 == try bits.readInt(1);
+                nibble_buf[nibble_count] = @truncate(try bits.readInt(4));
+                nibble_count += 1;
+                if (is_last) break;
+                if (nibble_buf.len == nibble_count) return error.LiteralOverflow;
             }
-            try contexts.append(.{
-                .packet = .{
-                    .count = 0,
-                    .limit = limit,
-                    .packmode = packmode,
-                    .opfn = opid.opFn(),
-                },
-                .literals = std.ArrayList(u64).init(alloc),
-            });
+            // convert to bytes
+            var i_byte: usize = 0;
+            var i_right = nibble_count - 1;
+            var byte_buf = std.mem.zeroes([8]u8);
+            const nibble_align: usize = if (0 == nibble_count % 2) 0 else 1;
+            const byte_count: usize = (nibble_count + nibble_align) / 2;
+            while (i_byte < byte_count) {
+                byte_buf[i_byte] = nibble_buf[i_right];
+                if (0 == i_right) break;
+                byte_buf[i_byte] |= @as(u8, nibble_buf[i_right - 1]) << 4;
+                i_byte += 1;
+                i_right -|= 2;
+            }
+            // read as big endian
+            var literal = std.mem.readInt(u64, &byte_buf, .little);
+            const bit_count: u16 = @truncate(bits.i - istart);
+            for (contexts.items) |*ctx| ctx.addBits(bit_count);
+            while (true) {
+                var last_ctx = &contexts.items[contexts.items.len - 1];
+                last_ctx.incrementSubCount();
+                try last_ctx.literals.append(literal);
+                if (!last_ctx.isComplete()) break;
+
+                literal = try last_ctx.complete();
+                _ = contexts.pop();
+                if (0 == contexts.items.len) {
+                    std.debug.print("= {}\n", .{literal});
+                    std.debug.print("∑ {}\n", .{version_sum});
+                    return;
+                }
+            }
             continue;
         }
 
-        // literal
-        var literal: u64 = 0;
-        var lsh: u6 = 60;
-        while (true) {
-            const is_last = 0 == try bits.readInt(1);
-            const seg = try bits.readInt(4);
-            literal += @as(u64, @intCast(seg)) << lsh;
-            if (!is_last and 0 == lsh) return error.LiteralOverflow;
-            if (is_last) break;
-            lsh -= 4;
-        }
+        // op
+        const opid = try std.meta.intToEnum(data.OpId, packet_type);
+        const packmode = try std.meta.intToEnum(
+            data.PackMode,
+            try bits.readInt(1),
+        );
+        const limit: u16 = switch (packmode) {
+            .npackets => try bits.readInt(11),
+            .nbits => try bits.readInt(15),
+        };
         const bit_count: u16 = @truncate(bits.i - istart);
-        for (contexts.items) |*ctx| ctx.addBits(bit_count);
-        var last_ctx = &contexts.items[contexts.items.len - 1];
-        last_ctx.incrementSubCount();
-        try last_ctx.literals.append(literal);
-        while (last_ctx.isComplete()) {
-            literal = try last_ctx.complete();
-            _ = contexts.pop();
-            if (0 == contexts.items.len) {
-                std.debug.print("= {d}\n", .{literal});
-                return;
-            }
-            last_ctx = &contexts.items[contexts.items.len - 1];
+        for (contexts.items) |*ctx| {
+            ctx.addBits(bit_count);
+            if (ctx.isComplete()) return error.PacketTruncated;
         }
+        try contexts.append(.{
+            .packet = .{
+                .count = 0,
+                .limit = limit,
+                .packmode = packmode,
+                .opfn = opid.opFn(),
+            },
+            .literals = std.ArrayList(u64).init(alloc),
+        });
     }
 }
